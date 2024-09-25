a5506e9ab43c659f6cc3a999731c8485db1dc1ae
Saif Maamun <sadifmamun@gmail.com>
2024-09-25 17:35:19 +0600

    polished [DONE]

--- src/app/modules/Auth/auth.validation.ts
+++ src/app/modules/Auth/auth.validation.ts
@@ -5,7 +5,7 @@ const signinValidationSchema = z.object({
   body: z.object({
     name: z.string().min(1, { message: 'Name is required.' }),
     email: z.string().email({ message: 'Invalid email format.' }),
-    password: z.string().min(1, { message: 'Password is required.' }),
+    password: z.string().min(8, { message: 'Password is required.' }),
     phone: z.string().min(1, { message: 'Phone number is required.' }),
     role: z.enum(['user', 'admin']).optional(),
     address: z.string().min(1, { message: 'Address is required.' }),
